2c2e612b5fea1b65e0005998adc0e88915671517
Sohrab Ali <modsohrabali@gmail.com>
2024-09-18 15:12:07 +0530

    Integrated Diamond-Price-Prediction into the main project

---
+++ Diamond-Price-Prediction/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sohrab Ali",
+    author_email="modsohrabali@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
