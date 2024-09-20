a6343ca696c5baef3607a62ea5429e772d975121
Sohrab Ali <modsohrabali@gmail.com>
2024-09-20 16:23:38 +0530

    Modified

--- Diamond-Price-Prediction/setup.py
+++ Diamond-Price-Prediction/setup.py
@@ -1,15 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="diamond_price_prediction",
+    name="src",
     version="0.0.1",
     author="Sohrab Ali",
     author_email="modsohrabali@gmail.com",
     packages=find_packages(),
-    install_requires=["streamlit",
-        "pandas",
-        "scikit-learn",
-        "matplotlib"],
-    python_requires=">=3.8",
+    install_requires=[],
 )
-
